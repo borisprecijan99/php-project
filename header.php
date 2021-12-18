b471383865e997f9a0d03f47d89104ccd39219da
borisprecijan99 <borisprecijanbpgmail.com>
2021-12-18 21:45:43 +0100

    Created header and footer for every page

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Steam Store</title>
+</head>
+<body class="store-background text-light">
+    <nav class="navbar navbar-expand-lg navbar-dark store-header-and-footer">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><img src="https://store.akamai.steamstatic.com/public/shared/images/header/logo_steam.svg?t=962016"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor01">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">REGISTER</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">LOGOUT</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-secondary" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </button>
+                </form>
+            </div>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("header.php"); ?>
+<div class="container center">
+    <div class="row">
+        <div class="col">
+            TEST
+        </div>
+    </div>
+</div>
+<?php include("footer.php"); ?>
